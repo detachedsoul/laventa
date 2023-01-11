8093348d472771c0e73350ce1060a83b7c45df0e
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2023-01-11 21:52:23 +0100

    chore(refactor): extracted categories filter accordion into reusable classes

--- components/checkout/Checkout.js
+++ components/checkout/Checkout.js
@@ -43,9 +43,91 @@ const Checkout = () => {
                             <input className="input-form border border-slate-200 rounded-lg border-solid focus:border-brand-dark-rose/[0.2]" type="email" name="email-address" id="email-address" />
                         </label>
 
+                        <div className="grid gap-4 lg:hidden lg:not-sr-only">
+                            <h2 className="header text-xl">
+                                Order summary
+                            </h2>
+
+                            <div className="divide-y divide-slate-200">
+                                <div className="flex items-center gap-4 pb-2">
+                                    <Image className="object-cover aspect-square rounded-md" src="/img/01.jpg" height={ 60 } width={ 60 } quality={ 100 } alt="Some Random Product" />
+
+                                    <div className="grid gap-0.5">
+                                        <h3 className="font-semibold leading-0">
+                                            UI Isometric Devices Pack
+                                        </h3>
+
+                                        <p className="text-brand-red slashed-zero">
+                                            $ <span className="font-medium">23.</span><small>00</small>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center gap-4 py-2">
+                                    <Image className="object-cover aspect-square rounded-md" src="/img/02.jpg" height={ 60 } width={ 60 } quality={ 100 } alt="Some Random Product" />
+
+                                    <div className="grid gap-0.5">
+                                        <h3 className="font-semibold leading-0">
+                                            UI Isometric Devices Pack
+                                        </h3>
+
+                                        <p className="text-brand-red slashed-zero">
+                                            $<span className="font-medium">23.</span><small>00</small>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center gap-4 py-2">
+                                    <Image className="object-cover aspect-square rounded-md" src="/img/03.jpg" height={ 60 } width={ 60 } quality={ 100 } alt="Some Random Product" />
+
+                                    <div className="grid gap-0.5">
+                                        <h3 className="font-semibold leading-0">
+                                            UI Isometric Devices Pack
+                                        </h3>
+
+                                        <p className="text-brand-red slashed-zero">
+                                            $<span className="font-medium">23.</span><small>00</small>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="py-4 grid gap-0.5 slashed-zero">
+                                    <div className="flex items-center gap-4 justify-between">
+                                        <p className="font-medium">
+                                            Subtotal:
+                                        </p>
+
+                                        <p>
+                                            $<span className="font-medium">23.</span><small>00</small>
+                                        </p>
+                                    </div>
+
+                                    <div className="flex items-center gap-4 justify-between">
+                                        <p className="font-medium">
+                                            Taxes:
+                                        </p>
+
+                                        <p>
+                                            $<span className="font-medium">9.</span><small>30</small>
+                                        </p>
+                                    </div>
+
+                                    <div className="flex items-center gap-4 justify-between text-xl">
+                                        <p className="font-medium">
+                                            Total:
+                                        </p>
+
+                                        <p>
+                                            $<span className="font-medium">9.</span><small>30</small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <div className="border border-slate-200 rounded-lg divide-y divide-slate-200 lg:col-span-2">
                             <div className="p-4">
-                                <button className="flex items-center justify-between gap-4 w-full font-semibold hover:text-brand-red" type="button">
+                                <button className="flex items-center justify-between gap-4 w-full font-semibold hover:text-brand-red bg-orange-500" type="button">
                                     <span>
                                         <i className="fr fi-rr-credit-card pr-2"></i>
                                         Pay with Credit Card
@@ -53,6 +135,12 @@ const Checkout = () => {
 
                                     <i className="fr fi-rr-angle-down text-base"></i>
                                 </button>
+
+                                <div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laudantium, laboriosam culpa ratione, magnam tempora cum architecto, mollitia dolorem possimus sint officiis doloremque eum recusandae similique exercitationem quos suscipit obcaecati!
+                                    </p>
+                                </div>
                             </div>
 
                             <div className="p-4">
@@ -69,7 +157,7 @@ const Checkout = () => {
                     </form>
                 </div>
 
-                <div className="grid gap-8 lg:col-span-4 lg:py-[6%] lg:px-[10%]">
+                <div className="hidden lg:grid gap-8 lg:col-span-4 lg:py-[6%] lg:px-[10%]">
                     <h2 className="header text-xl text-center">
                         Order summary
                     </h2>
@@ -117,7 +205,7 @@ const Checkout = () => {
                             </div>
                         </div>
 
-                        <div className="py-4 grid gap-0.5">
+                        <div className="py-4 grid gap-0.5 slashed-zero">
                             <div className="flex items-center gap-4 justify-between">
                                 <p className="font-medium">
                                     Subtotal:
@@ -139,7 +227,7 @@ const Checkout = () => {
                             </div>
                         </div>
 
-                        <div className="flex place-content-center py-4 text-3xl">
+                        <div className="flex place-content-center py-4 text-3xl slashed-zero">
                             $<span className="font-bold">9.</span><small className="font-medium">30</small>
                         </div>
                     </div>
