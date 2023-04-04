1018f84b3dabeb4e9be71b471daf5d3f394e0552
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2023-04-04 11:03:15 +0100

    chore(refactor): use swr to fetch products in categories page

--- app/categories/page.js
+++ app/categories/page.js
@@ -1,47 +1,75 @@
+"use client";
+
 import CategoryHero from "@components/categories/CategoryHero";
 import CategoryFilter from "@components/categories/CategoryFilter";
 import Products from "@components/categories/Products";
+import ProductsLoadingSkeleton from "@components/ProductsLoadingSkeleton";
+import usePaginate from "@store/usePaginate";
+import useFetch from "@helpers/useFetch";
 
-const fetchCategories = async () => {
-	const endpoint = "categories";
-	const url = process.env.NEXT_PUBLIC_API_URL + endpoint;
-	const req = await fetch(`${url}`);
+const fetchProducts = async (url) => {
+	const res = await fetch(url);
 
-	if (!req.ok) {
-		return `There was an error fetching the requested resource. Please make sure that the API endpoint ${endpoint} is correct.`;
-	} else {
-		const { data } = await req.json();
+	const data = await res.json();
 
-		return data;
-	}
+	return data;
 };
 
-const fetchProducts = async () => {
-	const endpoint = "products?populate=*";
-	const url = process.env.NEXT_PUBLIC_API_URL + endpoint;
-	const req = await fetch(`${url}`);
+const Page = () => {
+	// Fetch products thats fall under the specified category
+	const page = usePaginate((state) => state.page);
+	const productsArr = useFetch(
+		`${process.env.NEXT_PUBLIC_API_URL}products?pagination[pageSize]=3&pagination[page]=${page}&populate=*`,
+		fetchProducts,
+	).data;
 
-	if (!req.ok) {
-		return `There was an error fetching the requested resource. Please make sure that the API endpoint ${url} is correct.`;
-	} else {
-		const { data } = await req.json();
+	const isLoading = useFetch(
+		`${process.env.NEXT_PUBLIC_API_URL}products?pagination[pageSize]=3&pagination[page]=${page}&populate=*`,
+		fetchProducts,
+	).isLoading;
 
-		return data;
-	}
-};
+	const productsError = useFetch(
+		`${process.env.NEXT_PUBLIC_API_URL}products?pagination[pageSize]=3&pagination[page]=${page}&populate=*`,
+		fetchProducts,
+	).error;
 
-const Page = async () => {
-    const productsArr = await fetchProducts();
+	const nextPage = usePaginate((state) => state.nextPage);
+	const prevPage = usePaginate((state) => state.prevPage);
+	const toPage = usePaginate((state) => state.toPage);
 
-    return (
-        <>
-            <CategoryHero />
-            <CategoryFilter />
-            <main className="space-y-20 py-12 px-[3%]">
-                <Products productData={productsArr} />
-            </main>
-        </>
-    );
+	if (productsError)
+		return (
+			<p className="px-[3%] font-bold text-center text-xl mx-auto md:w-1/2 py-12">
+				There was an error fetching the requested resource. Please try
+				again later.
+			</p>
+		);
+
+	return (
+		<>
+			<CategoryHero />
+			<CategoryFilter />
+
+			<main className="space-y-20 py-12 px-[3%]">
+				{isLoading ? (
+					<ProductsLoadingSkeleton />
+				) : productsArr?.data?.length > 0 ? (
+					<Products
+						productData={productsArr}
+						page={page}
+						nextPage={nextPage}
+						prevPage={prevPage}
+						toPage={toPage}
+					/>
+				) : (
+					<p className="font-bold text-center text-xl mx-auto md:w-1/2">
+						There are no products under this category available yet.
+						Please check back at a later time.
+					</p>
+				)}
+			</main>
+		</>
+	);
 };
 
 export default Page;
